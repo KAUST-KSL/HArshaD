942fe41d9d35da1e3edcbe6b514aac217225eeea
Georgios Markomanolis <markomg@cori08-bond0.224>
2017-08-28 07:04:15 -0700

    the framework supports darshan 3 and defines paths for Shaheen and Cori

--- compare_darshan.sh
+++ compare_darshan.sh
@@ -7,9 +7,22 @@
 # Declare the darshan path where the logs are saved in the darshan_path variable
 # Dependency: pdfjam
 
-#!/bin/bash
+#!/bin/bash 
 
-export darshan_path=/project/logs/darshan/ 
+
+darshan_ver=`module avail --long 2>&1 | grep darshan | grep default | awk -F "[/ .]" '{print $2}'`
+name=`hostname`
+
+if [[ $name == *"cori"* ]]; then 
+
+export darshan_path="/global/cscratch1/sd/darshanlogs/"
+module load latex
+
+elif [[ $name == *"cdl"* ]]; then  
+export darshan_path="/project/logs/darshan/"
+
+fi
+
 
 file1=`ls -ltr $darshan_path/*/*/*/$USER*"_id"$1* | tail -n 1 |  awk '{print $9}'`
 file2=`ls -ltr $darshan_path/*/*/*/$USER*"_id"$2* | tail -n 1 |  awk '{print $9}'`
@@ -25,6 +38,9 @@ darshan-parser $file1 > temp_parser_$1
 darshan-parser $file2 > temp_parser_$2
 
 
+
+if [ $darshan_ver -eq 2 ]; then
+
 execu=`cat temp_parser_$1 | grep exe | awk '{print $(NF-2)}' | awk 'BEGIN{FS="/"} {print $NF}'`
 day_run1=`cat temp_parser_$1 | grep end_time_asci | awk '{print $5}'`
 year_run1=`cat temp_parser_$1 | grep end_time_asci | awk '{print $7}'`
@@ -32,26 +48,48 @@ month_run1=`cat temp_parser_$1 | grep end_time_asci | awk '{print $4}'`
 day_run2=`cat temp_parser_$2 | grep end_time_asci | awk '{print $5}'`
 year_run2=`cat temp_parser_$2 | grep end_time_asci | awk '{print $7}'`
 month_run2=`cat temp_parser_$2 | grep end_time_asci | awk '{print $4}'`
+pdf_fil=`echo $file | awk 'BEGIN{FS="/"} {print $NF}' | sed 's/darshan.gz/pdf/'`
+
+elif [ $darshan_ver -eq 3 ]; then
+execu=`cat temp_parser_$1 | grep exe | awk 'BEGIN{FS="[/ ]"} {print $(NF-1)}'`
+day_run1=`cat temp_parser_$1 | grep end_time_asci | awk '{print $5}'`
+year_run1=`cat temp_parser_$1 | grep end_time_asci | awk '{print $7}'`
+month_run1=`cat temp_parser_$1 | grep end_time_asci | awk '{print $4}'`
+day_run2=`cat temp_parser_$2 | grep end_time_asci | awk '{print $5}'`
+year_run2=`cat temp_parser_$2 | grep end_time_asci | awk '{print $7}'`
+month_run2=`cat temp_parser_$2 | grep end_time_asci | awk '{print $4}'`
+pdf_file=`echo $file | awk 'BEGIN{FS="/"} {print $NF}' | sed 's/darshan/darshan.pdf/'`
+
+fi
+
 
 month_run1_2=`date -d "1 $month_run1" "+%m"`
 month_run2_2=`date -d "1 $month_run2" "+%m"`
 
-pdf_file1=`echo $file1 | awk 'BEGIN{FS="/"} {print $NF}' | sed 's/darshan.gz/pdf/'`
-pdf_file2=`echo $file2 | awk 'BEGIN{FS="/"} {print $NF}' | sed 's/darshan.gz/pdf/'`
+pdf_file1=`echo $file1 | awk 'BEGIN{FS="/"} {print $NF}' | sed 's/darshan/darshan.pdf/'`
+pdf_file2=`echo $file2 | awk 'BEGIN{FS="/"} {print $NF}' | sed 's/darshan/darshan.pdf/'`
 
 pdfjam $pdf_file1 '1' $pdf_file2 '1' --nup 2x1 --landscape --outfile file1.pdf
 pdfjam $pdf_file1 '2' $pdf_file2 '2' --nup 2x1 --landscape --outfile file2.pdf
 pdfjam $pdf_file1 '3' $pdf_file2 '3' --nup 2x1 --landscape --outfile file3.pdf
 
-gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=compare_darshan_$1_$2.pdf file1.pdf file2.pdf file3.pdf
+if [ $darshan_ver -eq 3 ]; then
+pdfjam $pdf_file1 '4' $pdf_file2 '4' --nup 2x1 --landscape --outfile file4.pdf
 
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=compare_darshan_$1_$2.pdf file1.pdf file2.pdf file3.pdf file4.pdf
+rm file1.pdf file2.pdf file3.pdf file4.pdf
+
+else
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=compare_darshan_$1_$2.pdf file1.pdf file2.pdf file3.pdf 
+rm file1.pdf file2.pdf file3.pdf
+
+fi
 mkdir -p experiments/comparison/$execu/${year_run1}_${year_run2}/${month_run1_2}_${month_run2_2}/${day_run1}_${day_run2}/
 mv compare_darshan_$1_$2.pdf experiments/comparison/$execu/${year_run1}_${year_run2}/${month_run1_2}_${month_run2_2}/${day_run1}_${day_run2}/
 
-mv temp_parser_$1 experiments/comparison/$execu/${year_run1}_${year_run2}/${month_run1_2}_${month_run2_2}/${day_run1}_${day_run2}/
-mv temp_parser_$2 experiments/comparison/$execu/${year_run1}_${year_run2}/${month_run1_2}_${month_run2_2}/${day_run1}_${day_run2}/
+#mv temp_parser_$1 experiments/comparison/$execu/${year_run1}_${year_run2}/${month_run1_2}_${month_run2_2}/${day_run1}_${day_run2}/
+#mv temp_parser_$2 experiments/comparison/$execu/${year_run1}_${year_run2}/${month_run1_2}_${month_run2_2}/${day_run1}_${day_run2}/
 
 
-rm file1.pdf file2.pdf file3.pdf
 evince experiments/comparison/$execu/${year_run1}_${year_run2}/${month_run1_2}_${month_run2_2}/${day_run1}_${day_run2}/compare_darshan_$1_$2.pdf &
 
--- open_darshan.sh
+++ open_darshan.sh
@@ -7,9 +7,20 @@
 #2) To load darshan data from specific job, execute ./open_darshan.sh job_id
 # Declare the darshan path where the logs are saved in the darshan_path variable
 
-#!/bin/bash
+#!/bin/bash -x
 
+darshan_ver=`module avail --long 2>&1 | grep darshan | grep default | awk -F "[/ .]" '{print $2}'`
+name=`hostname`
+
+if [[ $name == *"cori"* ]]; then
+
+export darshan_path="/global/cscratch1/sd/darshanlogs/"
+module load latex
+
+elif [[ $name == *"cdl"* ]]; then 
 export darshan_path="/project/logs/darshan/"
+
+fi
 
 if [ $# -eq 0 ]; then
 
@@ -26,21 +37,32 @@ file=`ls -ltr $darshan_path/*/*/*/$USER*"_id"$1* | tail -n 1 |  awk '{print $9}'
 fi
 
 echo "Used Darshan file:"
-echo $file"\n"
+echo $file
 
 darshan-job-summary.pl $file
 darshan-parser $file > temp_parser
 
+if [ $darshan_ver -eq 2 ]; then
 execu=`cat temp_parser | grep exe | awk '{print $(NF-2)}' | awk 'BEGIN{FS="/"} {print $NF}'`
 day_run=`cat temp_parser | grep end_time_asci | awk '{print $5}'`
 year_run=`cat temp_parser | grep end_time_asci | awk '{print $7}'`
 month_run=`cat temp_parser | grep end_time_asci | awk '{print $4}'`
 
-month_run2=`date -d "1 $month_run" "+%m"`
-
 pdf_file=`echo $file | awk 'BEGIN{FS="/"} {print $NF}' | sed 's/darshan.gz/pdf/'`
+
+elif [ $darshan_ver -eq 3 ]; then
+execu=`cat temp_parser | grep exe | awk 'BEGIN{FS="[/ ]"} {print $(NF-1)}'`
+day_run=`cat temp_parser | grep end_time_asci | awk '{print $5}'`
+year_run=`cat temp_parser | grep end_time_asci | awk '{print $7}'`
+month_run=`cat temp_parser | grep end_time_asci | awk '{print $4}'`
+pdf_file=`echo $file | awk 'BEGIN{FS="/"} {print $NF}' | sed 's/darshan/darshan.pdf/'`
+
+fi
+
+month_run2=`date -d "1 $month_run" "+%m"`
+echo experiments/$execu/$year_run/$month_run2/$day_run/
 mkdir -p experiments/$execu/$year_run/$month_run2/$day_run/
 mv $pdf_file experiments/$execu/$year_run/$month_run2/$day_run/
-mv temp_parser experiments/$execu/$year_run/$month_run2/$day_run/
+#mv temp_parser experiments/$execu/$year_run/$month_run2/$day_run/
 
 evince experiments/$execu/$year_run/$month_run2/$day_run/$pdf_file &
